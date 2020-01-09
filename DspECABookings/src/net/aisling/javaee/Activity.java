1009ffa5e92390dfd113f9862f06ef212dfc5d78
aisling <aisling@192.168.0.80>
2020-01-09 12:23:36 +0000

    format update

--- DspECABookings/src/net/aisling/javaee/Activity.java
+++ DspECABookings/src/net/aisling/javaee/Activity.java
@@ -5,34 +5,34 @@ public class Activity {
 	private int aId;
 	private String dayOn;
 	private String activityName;
-    private String classTime;
-    private String eligibility;
-    private int noOfWeeks;
-    private int cost;
-    private int maxClassSize;
-    private int spacesAvailable;
-	
-    public Activity() {
+	private String classTime;
+	private String eligibility;
+	private int noOfWeeks;
+	private int cost;
+	private int maxClassSize;
+	private int spacesAvailable;
+
+	public Activity() {
 	}
 
 	public Activity(int aId) {
 		this.setaId(aId);
 	}
-	
-	public Activity(String dayOn,String activityName, String classTime, int cost  ) {
+
+	public Activity(String dayOn, String activityName, String classTime, int cost) {
 		this.dayOn = dayOn;
 		this.activityName = activityName;
 		this.classTime = classTime;
 		this.cost = cost;
 	}
-	
+
 	public Activity(String activityName) {
 		this.activityName = activityName;
 	}
 
-	public Activity(String dayOn, String activityName, String classTime, String eligibility,
-			int noOfWeeks, int cost, int maxClassSize, int spacesAvailable, int aId) {
-		
+	public Activity(String dayOn, String activityName, String classTime, String eligibility, int noOfWeeks, int cost,
+			int maxClassSize, int spacesAvailable, int aId) {
+
 		this.dayOn = dayOn;
 		this.activityName = activityName;
 		this.classTime = classTime;
@@ -43,9 +43,9 @@ public class Activity {
 		this.spacesAvailable = spacesAvailable;
 		this.setaId(aId);
 	}
-	
-	public Activity(String dayOn, String activityName, String classTime, String eligibility,
-			int noOfWeeks, int cost, int maxClassSize, int spacesAvailable) {
+
+	public Activity(String dayOn, String activityName, String classTime, String eligibility, int noOfWeeks, int cost,
+			int maxClassSize, int spacesAvailable) {
 		this.dayOn = dayOn;
 		this.activityName = activityName;
 		this.classTime = classTime;
@@ -55,8 +55,7 @@ public class Activity {
 		this.maxClassSize = maxClassSize;
 		this.spacesAvailable = spacesAvailable;
 	}
-    
-	
+
 	public int getaId() {
 		return aId;
 	}
@@ -68,42 +67,55 @@ public class Activity {
 	public String getActivityName() {
 		return activityName;
 	}
+
 	public void setActivityName(String activityName) {
 		this.activityName = activityName;
 	}
+
 	public String getClassTime() {
 		return classTime;
 	}
+
 	public void setClassTime(String classTime) {
 		this.classTime = classTime;
 	}
+
 	public String getEligibility() {
 		return eligibility;
 	}
+
 	public void setEligibility(String eligibility) {
 		this.eligibility = eligibility;
 	}
+
 	public int getNoOfWeeks() {
 		return noOfWeeks;
 	}
+
 	public void setNoOfWeeks(int noOfWeeks) {
 		this.noOfWeeks = noOfWeeks;
 	}
+
 	public int getCost() {
 		return cost;
 	}
+
 	public void setCost(int cost) {
 		this.cost = cost;
 	}
+
 	public int getMaxClassSize() {
 		return maxClassSize;
 	}
+
 	public void setMaxClassSize(int maxClassSize) {
 		this.maxClassSize = maxClassSize;
 	}
+
 	public int getSpacesAvailable() {
 		return spacesAvailable;
 	}
+
 	public void setSpacesAvailable(int spacesAvailable) {
 		this.spacesAvailable = spacesAvailable;
 	}
@@ -117,6 +129,6 @@ public class Activity {
 	}
 
 	public String toString() {
-		return activityName +" ";
-	}	
+		return activityName + " ";
+	}
 }
